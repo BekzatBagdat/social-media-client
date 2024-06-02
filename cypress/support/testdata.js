7845ed8a0818a0a989a0bfd16b1cae3a497661d2
Bekzat Bagdat <paintdex12@gmail.com>
2024-06-02 20:27:18 +0200

    installed and configured cypress, created userdata js for user credentials testing

---
+++ cypress/support/testdata.js
@@ -0,0 +1,9 @@
+// User credentials for testing
+
+const validEmail = 'valid@gmail.no';
+const validPassword = 'validPassword';
+
+const invalidEmail = 'invalid@gmail.com';
+const invalidPassword = 'invalidPassword';
+
+export { validEmail, validPassword, invalidEmail, invalidPassword };
