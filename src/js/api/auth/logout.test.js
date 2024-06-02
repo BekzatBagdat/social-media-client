7b471c30c9b024dcc66c6df99512502b323f836a
Bekzat Bagdat <paintdex12@gmail.com>
2024-06-02 17:43:21 +0200

    unit tests for logout

---
+++ src/js/api/auth/logout.test.js
@@ -0,0 +1,25 @@
+import { logout } from './logout';
+
+// Mocking the localstorage remove func
+const localStorageMock = {
+  removeItem: jest.fn(),
+};
+
+/// Assignning localStorageMock to the actual localstorage
+global.localStorage = localStorageMock;
+
+describe('logging out', () => {
+  beforeEach(() => {
+    // Clearing the local storage mock before running the test
+    jest.clearAllMocks();
+  });
+
+  it('logsout the user and removes the token from localStorage', () => {
+    // Calling the logout function
+    logout();
+    // Expecting removal of the token from localStorage
+    expect(localStorage.removeItem).toHaveBeenCalledWith('token');
+    // Expecting removal of the profile from localStorage
+    expect(localStorage.removeItem).toHaveBeenCalledWith('profile');
+  });
+});
